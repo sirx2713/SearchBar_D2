bdf1796f80c27b31e3dfc51c10aa73280f0cc4fb
Mabende <belindamabende@gmail.com>
2024-06-07 15:21:09 +0200

    Initial Commit

---
+++ SearchBar_D2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SearchBar_D2
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/06/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SearchBar_D2/SearchBar_D2App.swift
@@ -0,0 +1,17 @@
+//
+//  SearchBar_D2App.swift
+//  SearchBar_D2
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/06/07.
+//
+
+import SwiftUI
+
+@main
+struct SearchBar_D2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
